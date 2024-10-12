607335bba9c3aabd25b1a94cbfb323bdf4761337
Abhishek Singh <abhi@Abhisheks-MacBook-Pro.local>
2024-10-13 02:59:30 +0530

    traffic light

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [active, setActive] = useState("red");
+
+  console.log(active);
+  useEffect(() => {
+    const intervals = {
+      red: 4000,
+      yellow: 3000,
+      green: 2000,
+    };
+    let timer = setTimeout(() => {
+      setActive((prev) => {
+        if (prev === "green") {
+          return "red";
+        } else if (prev === "red") {
+          return "yellow";
+        } else {
+          return "green";
+        }
+      });
+    }, intervals[active]);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [active]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{ fontSize: "16px" }}>Traffic Light</h1>
+      <div
+        style={{
+          height: "200px",
+          width: "80px",
+          backgroundColor: "black",
+          borderRadius: "10px",
+          display: "flex",
+          justifyContent: "center",
+          padding: "6px",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: "6px",
+        }}
+      >
+        <div
+          style={{
+            height: "60px",
+            width: "60px",
+            backgroundColor: "red",
+            borderRadius: "50%",
+            opacity: active === "red" ? 1 : 0.3,
+          }}
+        ></div>
+        <div
+          style={{
+            height: "60px",
+            width: "60px",
+            backgroundColor: "yellow",
+            borderRadius: "50%",
+            opacity: active === "yellow" ? 1 : 0.3,
+          }}
+        ></div>
+        <div
+          style={{
+            height: "60px",
+            width: "60px",
+            backgroundColor: "green",
+            borderRadius: "50%",
+            opacity: active === "green" ? 1 : 0.3,
+          }}
+        ></div>
+      </div>
     </div>
   );
 }
